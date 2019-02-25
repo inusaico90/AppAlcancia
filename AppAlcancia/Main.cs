68645a4def25a626fcb3462a84e9df779b29d228
inusaico90 <erneyjj@hotmail.es>
2019-02-25 17:48:09 -0500

    Carpeta AppAlcancia
    En esta carpeta se va el Main del programa

---
+++ AppAlcancia/Main.cs
@@ -0,0 +1,13 @@
+﻿using CapaServicios.ControlesConsola.Menu;
+
+namespace appAlcancia.CapaPresentacion.Consola
+{
+    class Principal
+    {
+        static void Main(string[] args){
+            string[] varVectorOpciones = new string[6]
+            {"Menú","\tRegistrar moneda","\tEliminar moneda","\tImprimir lista de monedas","\tPrueba sub-menú","\tSalir"};
+            new ClsMenuPrincipal(varVectorOpciones);
+        }
+    }
+}
